31eb9855249ee45f2c48cb025d57e768e4b2b618
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-12-12 09:34:48 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
